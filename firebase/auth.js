58e78905bb908d56c35f6ac6ae31e8068d8a1c70
SHUBHAM KUMAR JHA <shubhamkumarjha@SHUBHAMs-MacBook-Air.local>
2026-04-04 16:44:16 +0530

    firebase

---
+++ firebase/auth.js
@@ -0,0 +1,42 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "./firebase";
+
+// SIGN UP
+export const signupUser = async (name, email, password) => {
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+
+  const user = userCredential.user;
+
+  await setDoc(doc(db, "users", user.uid), {
+    name,
+    email,
+    role: "citizen",
+    createdAt: new Date(),
+  });
+
+  return user;
+};
+
+// LOGIN
+export const loginUser = async (email, password) => {
+  const userCredential = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+  return userCredential.user;
+};
+
+// LOGOUT
+export const logoutUser = async () => {
+  await signOut(auth);
+};
